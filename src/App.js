2971f4bcc591fb64bdcd898ca6fc1e2b567e518d
SURAJGUPTA0414 <93039172+SURAJGUPTA0414@users.noreply.github.com>
2022-02-22 17:01:01 +0530

    reactprc
    reactprc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./components/Style.css"
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Masthead from "./components/Masthead";
+import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
+import Footer from "./components/Footer";
+import Copyright from "./components/Copyright";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    return(
+        <div>
+<Navbar/>
+<Masthead/>
+<Portfolio/>
+<About/>
+<Contact/>
+<Footer/>
+<Copyright/>
+
+        </div>
+    )
 }
+    
+        
+    
 
 export default App;
